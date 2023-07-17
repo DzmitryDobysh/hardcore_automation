ae64e5fba59a79face641a7a957039611a514573
Dzmitry Dobysh <Dzmitry_Dobysh@epam.com>
2023-07-18 02:03:44 +0400

    feat. added simplified config & test data

--- src/test/java/service/FormDefaultModelCreator.java
+++ src/test/java/service/FormDefaultModelCreator.java
@@ -3,9 +3,11 @@ package service;
 import model.FormDefaultModel;
 
 public class FormDefaultModelCreator {
+
+    public static final String INSTANCES = "testdata.instances";
     public static FormDefaultModel createDefaultFormModel() {
         FormDefaultModel formModel = new FormDefaultModel();
-        formModel.setInstances("4");
+        formModel.setInstances(TestDataReader.getTestData(INSTANCES));
         return formModel;
     }
 }
