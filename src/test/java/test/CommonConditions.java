0d62c6fd0e3245b6436fdcf42855a91384096340
Dzmitry Dobysh <Dzmitry_Dobysh@epam.com>
2023-07-18 21:55:38 +0400

    feat. added creating of screenshots folder, regarding of test status

--- src/test/java/test/CommonConditions.java
+++ src/test/java/test/CommonConditions.java
@@ -6,17 +6,30 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 
+import java.io.File;
+
 @Listeners({util.TestListener.class})
 public class CommonConditions {
     protected WebDriver driver;
 
     @BeforeTest(alwaysRun = true)
     public void browserSetup() {
+        createScreenshotsFolder();
         driver = DriverSingleton.getDriver();
     }
 
     @AfterTest(alwaysRun = true)
     public void browserExit() {
         DriverSingleton.closeDriver();
+    }
+
+    private void createScreenshotsFolder() {
+        File screenshotsFolder = new File(".//target/screenshots");
+        boolean folderCreated = screenshotsFolder.mkdirs();
+        if (folderCreated || screenshotsFolder.exists()) {
+            System.out.println("Screenshots folder created successfully.");
+        } else {
+            System.err.println("Failed to create screenshots folder.");
+        }
     }
 }
