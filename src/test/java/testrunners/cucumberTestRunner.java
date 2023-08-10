6764ac63e2f492a2b4983b0f8fb339b44432fd08
Dzmitry Dobysh <Dzmitry_Dobysh@epam.com>
2023-08-10 23:44:56 +0400

    Revert "initial BDD commit"
    This reverts commit fb17fbdf6b8e93eb537e04deca9e1a31e1391bd4.

--- src/test/java/testrunners/cucumberTestRunner.java
+++
@@ -1,8 +0,0 @@
-package testrunners;
-
-import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", plugin = "pretty")
-public class cucumberTestRunner extends AbstractTestNGCucumberTests {
-}
