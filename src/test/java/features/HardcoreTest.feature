fb17fbdf6b8e93eb537e04deca9e1a31e1391bd4
Dzmitry Dobysh <Dzmitry_Dobysh@epam.com>
2023-08-10 23:40:28 +0400

    initial BDD commit

---
+++ src/test/java/features/HardcoreTest.feature
@@ -0,0 +1,15 @@
+Feature: Hardcore Test
+
+
+  Scenario: Open google cloud calculator page
+    Given I open google cloud main page
+    When I perform a search for "Google Cloud Platform Pricing Calculator" page
+    Then Click on cloud calculator page link
+
+    When I enter all info with following data:
+      | Instances | OS   | Instance Class | Series | Machine Type                           | GPUs | GPU Type          | SSD | Location     | Usage |
+      | 4         | free | regular        | n1     | CP-COMPUTEENGINE-VMIMAGE-N1-STANDARD-8 | 1    | NVIDIA_TESLA_V100 | 2   | europe-west3 | 1     |
+    Then All entered info is present and added to estimation
+
+    When I created temp email and send estimation to it
+    Then I check the total cost in the email against the calculated total cost
--- src/test/java/model/FormDefaultModel.java
+++ src/test/java/model/FormDefaultModel.java
@@ -1,6 +1,18 @@
 package model;
 
 public class FormDefaultModel {
+    private String instances;
+    private String os;
+    private String instanceClass;
+    private String series;
+    private String machineType;
+    private String gpus;
+    private String gpuType;
+    private String ssd;
+    private String location;
+    private String usage;
+
+
     public String getInstances() {
         return instances;
     }
@@ -9,5 +21,76 @@ public class FormDefaultModel {
         this.instances = instances;
     }
 
-    private String instances;
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getInstanceClass() {
+        return instanceClass;
+    }
+
+
+    public void setInstanceClass(String instanceClass) {
+        this.instanceClass = instanceClass;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public String getMachineType() {
+        return machineType;
+    }
+
+    public void setMachineType(String machineType) {
+        this.machineType = machineType;
+    }
+
+    public String getGpus() {
+        return gpus;
+    }
+
+    public void setGpus(String gpus) {
+        this.gpus = gpus;
+    }
+
+    public String getGpuType() {
+        return gpuType;
+    }
+
+    public void setGpuType(String gpuType) {
+        this.gpuType = gpuType;
+    }
+
+    public String getSsd() {
+        return ssd;
+    }
+
+    public void setSsd(String ssd) {
+        this.ssd = ssd;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getUsage() {
+        return usage;
+    }
+
+    public void setUsage(String usage) {
+        this.usage = usage;
+    }
 }
