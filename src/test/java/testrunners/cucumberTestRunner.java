95eee7c5bdf41b2c8a3d891f2cd6929a344d0a5f
Dzmitry Dobysh <Dzmitry_Dobysh@epam.com>
2023-08-11 19:16:07 +0400

    added html and json reports

--- src/test/java/testrunners/cucumberTestRunner.java
+++ src/test/java/testrunners/cucumberTestRunner.java
@@ -1,8 +1,13 @@
 package testrunners;
 
-import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", plugin = "pretty")
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = "stepDefinitions",
+        plugin = {"pretty",
+                "html:target/SystemTestReports/html/report.html",
+                "json:target/SystemTestReports/json/report.json"})
 public class cucumberTestRunner extends AbstractTestNGCucumberTests {
 }
